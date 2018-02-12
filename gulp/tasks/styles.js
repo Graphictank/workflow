db74065e93e9573c7548f1c7691302575072f25a
Steve <stehan.com@gmail.com>
2018-02-11 22:29:29 -0500

    Main Header and Webpack

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,13 +2,14 @@ var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
+    hexcolors = require('postcss-hexrgba'),
     nested = require('postcss-nested'),
     cssImport = require('postcss-import'),
     mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, hexcolors, nested, autoprefixer]))
         .on('error', function (errorInfo){
             console.log(errorInfo.toString());
             this.emit('end');
