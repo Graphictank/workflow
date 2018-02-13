26e5e67f6a38e8c35b09dcf3a150ae99e8867dbd
Steve <stehan.com@gmail.com>
2018-02-13 01:07:26 -0500

    Completed Sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
+
 
 var mobileMenu = new MobileMenu();
 var revealOnScroll = new RevealOnScroll();
+var stickyHeader = new StickyHeader();
